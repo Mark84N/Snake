00951c27c5e0a26176f2ecbc9be71ad84a1aa12c
Mark84N <desiren77@gmail.com>
2016-07-29 07:43:40 +0300

    spacings in main
    made adjustments in spacings and set braces to correct positions in
    main

--- main.cpp
+++ main.cpp
@@ -1,41 +1,45 @@
 #pragma comment(lib, "user32.lib")
-#include <field.h>
 
 #include <Windows.h>
 #include <chrono>
 #include <thread>
 
-int main(int argc, char *argv[]){
+#include <field.h>
 
+int main(int argc, char *argv[])
+{
     Field f;
     f.showField();
 
-    while (true) {
+    while (true)
+    {
             system("cls");
 
             DIRECTIONS defaultDir = f.getSnakeDirection();
 
-            if (GetAsyncKeyState(VK_RIGHT)){
+            if (GetAsyncKeyState(VK_RIGHT))
+            {
                 keybd_event(VK_RIGHT, 0, KEYEVENTF_KEYUP, 0);
                 defaultDir = DIRECTIONS::RIGHT;
 
                 f.makeStep(DIRECTIONS::RIGHT);
-
             }
-            else if (GetAsyncKeyState(VK_LEFT)){
+            else if (GetAsyncKeyState(VK_LEFT))
+            {
                 keybd_event(VK_LEFT, 0, KEYEVENTF_KEYUP, 0);
                 defaultDir = DIRECTIONS::LEFT;
 
                 f.makeStep(DIRECTIONS::LEFT);
-
             }
-            else if (GetAsyncKeyState(VK_UP)){
+            else if (GetAsyncKeyState(VK_UP))
+            {
                 keybd_event(VK_UP, 0, KEYEVENTF_KEYUP, 0);
                 defaultDir = DIRECTIONS::UP;
 
                 f.makeStep(DIRECTIONS::UP);
             }
-            else if (GetAsyncKeyState(VK_DOWN)){
+            else if (GetAsyncKeyState(VK_DOWN))
+            {
                 keybd_event(VK_DOWN, 0, KEYEVENTF_KEYUP, 0);
                 defaultDir = DIRECTIONS::DOWN;
 
